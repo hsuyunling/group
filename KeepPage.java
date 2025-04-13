6ca68bccdfcece2c4f607e970720a0a77e59a803
Ls1315 <annie77230646@gmail.com>
2025-04-13 13:16:33 +0800

    Update KeepPage.java

--- KeepPage.java
+++ KeepPage.java
@@ -5,12 +5,14 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class KeepPage extends JFrame {
-
+	private static final int FRAME_WIDTH = 650;
+	private static final int FRAME_HEIGHT = 750;
+	
     public KeepPage() {
     	
     	// set frame
         setTitle("收藏頁面");
-        setSize(650, 750);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
         setLayout(new BorderLayout());
 		
         JPanel activityPanel = new JPanel();
