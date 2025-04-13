67a3323667a527a09d87a5b21a122185d0b6f036
Ls1315 <annie77230646@gmail.com>
2025-04-13 13:20:34 +0800

    Update KeepPage.java

--- KeepPage.java
+++ KeepPage.java
@@ -7,21 +7,20 @@ import java.util.ArrayList;
 public class KeepPage extends JFrame {
 	private static final int FRAME_WIDTH = 650;
 	private static final int FRAME_HEIGHT = 750;
-	
-    public KeepPage() {
+	public KeepPage() {
     	
-    	// set frame
-        setTitle("收藏頁面");
-        setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        setLayout(new BorderLayout());
-		
-        JPanel activityPanel = new JPanel();
-        activityPanel.setLayout(new BoxLayout(activityPanel, BoxLayout.Y_AXIS));
-
-        JScrollPane scrollPane = new JScrollPane(activityPanel);
-        add(scrollPane, BorderLayout.CENTER);
-
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setVisible(true);
-    }
+	   	// set frame
+		setTitle("收藏頁面");
+	        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	        setLayout(new BorderLayout());
+			
+	        JPanel activityPanel = new JPanel();
+	        activityPanel.setLayout(new BoxLayout(activityPanel, BoxLayout.Y_AXIS));
+	
+	        JScrollPane scrollPane = new JScrollPane(activityPanel);
+	        add(scrollPane, BorderLayout.CENTER);
+	
+	        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	        setVisible(true);
+	}
 }
