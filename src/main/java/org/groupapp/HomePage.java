3d434c7645b5cd334fe20e1af52303d12e876fc4
yunlinghsu <113306005@g.nccu.edu.tw>
2025-04-30 12:31:54 +0800

    Change buttons' style

--- src/main/java/org/groupapp/HomePage.java
+++ src/main/java/org/groupapp/HomePage.java
@@ -1,11 +1,14 @@
-
 package org.groupapp;
+
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
+import javax.swing.ButtonModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,7 +23,9 @@ public class HomePage extends JFrame {
     private JButton home, following, addActivity, personalInfo, searchBtn;
     JPanel actListPanel, followingPanel, addNew, personalPanel;
     JTextField searchBar;
-
+    Font font = new Font("Arial", Font.PLAIN, 20);
+    ArrayList<JButton> btns = new ArrayList<>();
+// -------------constructor-------------
     public HomePage() {
         setLayout(new BorderLayout());
         setTitle("Group");
@@ -40,7 +45,7 @@ public class HomePage extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    //頁面最上方
+ //-------------頁面最上方-------------
     public void createNorthPanel() {
         northPanel = new JPanel();
         searchBar = new JTextField(10);
@@ -54,7 +59,7 @@ public class HomePage extends JFrame {
         add(northPanel, BorderLayout.NORTH);
     }
 
-    //頁面中間
+//-------------頁面中間--------------
     public void createCenterPanel() {
         centerPanel = new JPanel();
         addNew = new EditPanel();
@@ -64,39 +69,68 @@ public class HomePage extends JFrame {
 
         centerPanel.setLayout(cardLayout);
         centerPanel.add(actListPanel, "home");
-        centerPanel.add(followingPanel, "following");
         centerPanel.add(addNew, "addNew");
+        centerPanel.add(followingPanel, "following");
         centerPanel.add(personalPanel, "my");
 
         add(centerPanel, BorderLayout.CENTER);
 
     }
 
-    //頁面最下方
+ //-------------頁面最下方-------------
     public void createSouthPanel() {
         southPanel = new JPanel();
-        southPanel.setLayout(new GridLayout(1,4));
+        southPanel.setLayout(new GridLayout(1, 4));
         home = new JButton("home");
         following = new JButton("following");
         addActivity = new JButton("+");
         personalInfo = new JButton("My");
+        btns.add(home);
+        btns.add(addActivity);
+        btns.add(following);
+        btns.add(personalInfo);
+        setBtnStyle();
 
-        southPanel.add(home);
-        southPanel.add(following);
-        southPanel.add(addActivity);
-        southPanel.add(personalInfo);
-
-        southPanel.setBackground(Color.GRAY);
         southPanel.setPreferredSize(new Dimension(650, 60));
 
         add(southPanel, BorderLayout.SOUTH);
 
     }
-
+// -------------btn settings-------------
     public void setBtnActionListener(JButton btn, String s) {
-        btn.addActionListener(e ->{
+        btn.addActionListener(e -> {
             cardLayout.show(centerPanel, s);
         });
 
+    }
+
+    public void setBtnStyle(){
+        Color normalColor = new Color(246, 209, 86);
+        Color pressedColor = new Color(195, 170, 87);
+
+
+        for (JButton btn : btns) {
+            final JButton thisBtn = btn; // ★ 每個 listener 各自有一個 btn 實例
+            thisBtn.setOpaque(true);
+            thisBtn.setBorderPainted(false);
+            thisBtn.setContentAreaFilled(true);
+            thisBtn.setFocusPainted(false);
+
+            thisBtn.setBackground(normalColor);
+            thisBtn.setForeground(Color.BLACK);
+
+            thisBtn.getModel().addChangeListener(e -> {
+                ButtonModel model = thisBtn.getModel();
+                if (model.isPressed()) {
+                    thisBtn.setBackground(pressedColor);
+                    thisBtn.setForeground(Color.WHITE);
+                } else {
+                    thisBtn.setBackground(normalColor);
+                    thisBtn.setForeground(Color.BLACK);
+
+                }
+            });
+            southPanel.add(thisBtn);
+        }
     }
 }
